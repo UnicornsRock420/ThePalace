7a1cb0ac02f98effce497a5bf152ec5e57238237
progmanpaul <progmanpaul@gmail.com>
2019-08-24 11:13:04 -0500

    Add files via upload

--- ThePalace.Core.Database/Build/5. Functions/5.00.Admin.ComputeCrc.sql
+++ ThePalace.Core.Database/Build/5. Functions/5.00.Admin.ComputeCrc.sql
@@ -1,17 +1,18 @@
 IF EXISTS(SELECT TOP 1 1 FROM SYS.ALL_OBJECTS WHERE OBJECT_ID = OBJECT_ID('[Admin].[ComputeCrc]')) BEGIN
 	DROP FUNCTION [Admin].[ComputeCrc]
 
-	PRINT 'DROPPING [Admin].[ComputeCrc]'
+	PRINT 'Dropping [Admin].[ComputeCrc]'
 END
-PRINT 'Creating [Admin].[ComputeCrc]'
 GO
-CREATE FUNCTION [Admin].[ComputeCrc]
-(
-	@Data [varbinary](max),
-	@Offset INT,
-	@isAsset [bit]
-)
-RETURNS INT
-AS
-	EXTERNAL NAME [ThePalace.SqlFunctions].[Cipher].[ComputeCrc]
+
+IF EXISTS(SELECT TOP 1 1 FROM SYS.ASSEMBLY_FILES WHERE NAME = 'ThePalace.SqlFunctions') BEGIN
+	EXEC (N'CREATE FUNCTION [Admin].[ComputeCrc](
+		@Data [varbinary](max),
+		@Offset INT,
+		@isAsset [bit]
+	) RETURNS INT
+	AS EXTERNAL NAME [ThePalace.SqlFunctions].[Cipher].[ComputeCrc]')
+
+	PRINT N'Created [Admin].[ComputeCrc]'
+END
 GO
--- ThePalace.Core.Database/Build/5. Functions/5.01.Admin.GetHex.sql
+++ ThePalace.Core.Database/Build/5. Functions/5.01.Admin.GetHex.sql
@@ -1,15 +1,16 @@
 IF EXISTS(SELECT TOP 1 1 FROM SYS.ALL_OBJECTS WHERE OBJECT_ID = OBJECT_ID('[Admin].[GetHex]')) BEGIN
 	DROP FUNCTION [Admin].[GetHex]
 
-	PRINT 'DROPPING [Admin].[GetHex]'
+	PRINT 'Dropping [Admin].[GetHex]'
 END
-PRINT 'Creating [Admin].[GetHex]'
 GO
-CREATE FUNCTION [Admin].[GetHex]
-(
-	@Source INT
-)
-RETURNS NVARCHAR(MAX)
-AS
-	EXTERNAL NAME [ThePalace.SqlFunctions].[Utility].[GetHex]
+
+IF EXISTS(SELECT TOP 1 1 FROM SYS.ASSEMBLY_FILES WHERE NAME = 'ThePalace.SqlFunctions') BEGIN
+	EXEC (N'CREATE FUNCTION [Admin].[GetHex](
+		@Source INT
+	) RETURNS NVARCHAR(MAX)
+	AS EXTERNAL NAME [ThePalace.SqlFunctions].[Utility].[GetHex]')
+
+	PRINT N'Created [Admin].[GetHex]'
+END
 GO

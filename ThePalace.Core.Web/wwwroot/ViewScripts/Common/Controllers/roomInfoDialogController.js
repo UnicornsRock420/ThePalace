0d57f5ac50184f9240ba763f632cf4943730294b
progmanpaul <progmanpaul@gmail.com>
2019-08-25 06:33:37 -0500

    Add files via upload

--- ThePalace.Core.Web/wwwroot/ViewScripts/Common/Controllers/roomInfoDialogController.js
+++ ThePalace.Core.Web/wwwroot/ViewScripts/Common/Controllers/roomInfoDialogController.js
@@ -1,7 +1,59 @@
-﻿angular.module('ThePalace').controller('roomInfoDialogController', ['$scope', 'model', function ($scope, model) {
+﻿angular.module('ThePalace').controller('roomInfoDialogController', ['$scope', 'model', 'RoomFlags', function ($scope, model, RoomFlags) {
     $scope.model = model;
 
+    $scope.flags = {};
+
+    if (($scope.model.flags & RoomFlags.RF_Private) != 0) {
+        $scope.flags.Private = true;
+    }
+
+    if (($scope.model.flags & RoomFlags.RF_NoPainting) != 0) {
+        $scope.flags.NoPainting = true;
+    }
+
+    if (($scope.model.flags & RoomFlags.RF_CyborgFreeZone) != 0) {
+        $scope.flags.NoScripts = true;
+    }
+
+    if (($scope.model.flags & RoomFlags.RF_Hidden) != 0) {
+        $scope.flags.Hidden = true;
+    }
+
+    if (($scope.model.flags & RoomFlags.RF_WizardsOnly) != 0) {
+        $scope.flags.StaffOnly = true;
+    }
+
+    if (($scope.model.flags & RoomFlags.RF_DropZone) != 0) {
+        $scope.flags.DropZone = true;
+    }
+
     $scope.Save_OnClick = (function ($event) {
+        $scope.model.flags = 0;
+
+        if ($scope.flags.Private) {
+            $scope.model.flags |= RoomFlags.RF_Private;
+        }
+
+        if ($scope.flags.NoPainting) {
+            $scope.model.flags |= RoomFlags.RF_NoPainting;
+        }
+
+        if ($scope.flags.NoScripts) {
+            $scope.model.flags |= RoomFlags.RF_CyborgFreeZone;
+        }
+
+        if ($scope.flags.Hidden) {
+            $scope.model.flags |= RoomFlags.RF_Hidden;
+        }
+
+        if ($scope.flags.StaffOnly) {
+            $scope.model.flags |= RoomFlags.RF_WizardsOnly;
+        }
+
+        if ($scope.flags.DropZone) {
+            $scope.model.flags |= RoomFlags.RF_DropZone;
+        }
+
         $scope.$close($scope.model);
     });
 }]);

28c651aa2b3be7796b02431b8c1e2cba09c903f9
progmanpaul <progmanpaul@gmail.com>
2019-08-25 09:19:20 -0500

    Add files via upload

---
+++ ThePalace.Core.Server/Protocols/Rooms/MSG_ROOMINFO.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.ComponentModel;
+using ThePalace.Core.Factories;
+using ThePalace.Core.Interfaces;
+using ThePalace.Core.Types;
+
+namespace ThePalace.Server.Protocols
+{
+    [Description("ofNr")]
+    public struct MSG_ROOMINFO : IReceiveProtocol
+    {
+        public RoomRec room;
+
+        public void Deserialize(Packet packet)
+        {
+        }
+
+        public void DeserializeJSON(string json)
+        {
+            var jsonResponse = (dynamic)null;
+
+            try
+            {
+                jsonResponse = (dynamic)JsonConvert.DeserializeObject<JObject>(json);
+
+                room = new RoomRec();
+                room.roomID = jsonResponse.roomID;
+                room.roomName = jsonResponse.name;
+                room.roomPicture = jsonResponse.pictName;
+                room.roomArtist = jsonResponse.artistName;
+                room.facesID = jsonResponse.facesID;
+                room.roomFlags = jsonResponse.flags;
+            }
+            catch
+            {
+            }
+        }
+    }
+}

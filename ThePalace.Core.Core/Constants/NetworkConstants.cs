8b09e8d9d2bb37a1b03b747453de68d8c790b395
progmanpaul <progmanpaul@gmail.com>
2019-08-27 18:03:46 -0500

    Add files via upload

--- ThePalace.Core.Core/Constants/NetworkConstants.cs
+++ ThePalace.Core.Core/Constants/NetworkConstants.cs
@@ -2,10 +2,10 @@
 {
     public static class NetworkConstants
     {
-        public const int AEPHIX_PACKET_BUFFER_SIZE = 4096;
-        public const int PALACE_PACKET_BUFFER_SIZE = 4096;
-        public const int ASSET_STREAM_BUFFER_SIZE = 4096;
+        public const int AEPHIX_PACKET_BUFFER_SIZE = 32768;
+        public const int PALACE_PACKET_BUFFER_SIZE = 32768;
+        public const int ASSET_STREAM_BUFFER_SIZE = 32768;
         public const int ASSET_MAX_BLOCK_SIZE = 9000; // That's right if it's over 9000!!! Get it?
-        public const int FILE_STREAM_BUFFER_SIZE = 4096;
+        public const int FILE_STREAM_BUFFER_SIZE = 32768;
     }
 }

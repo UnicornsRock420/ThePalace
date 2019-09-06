278d2b3d4954e5d28575a718cbb69c2bf0cb7d38
progmanpaul <progmanpaul@gmail.com>
2019-09-05 19:08:47 -0500

    Add files via upload

---
+++ ThePalace.Core.Core/Interfaces/IProtocolAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ThePalace.Core.Interfaces
+{
+    public interface IProtocolAttribute
+    {
+        bool OnBeforeProtocolExecute(Dictionary<string, object> contextValues);
+    }
+}
